ff5ce05fd3728c426923288709f801885eb3c4f5
Tarik366 <tariky113@gmail.com>
2026-08-21 16:20:38 +0300

    Fixed dumbass Windows terminal shit

--- bicomic/bicomic.cpp
+++ bicomic/bicomic.cpp
@@ -6,13 +6,23 @@
 #include <interpretter.h>
 #include <fileManager.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif // _WIN32
+
 using namespace std;
 
 int main()
 {
-    string exampleText = readTextFile("example.bcs");
+#if _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif // _WIN32
+
+    string exampleText = readTextFile("./example.bcs");
     auto cascadian = removeComments(exampleText);
 
-    cout << cascadian << endl;
+    auto parsed = parseBody(cascadian);
+
+    cout << parsed << endl;
     return 0;
 }
