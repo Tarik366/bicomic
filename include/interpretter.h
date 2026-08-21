d2ed7a2915284ec7e07d7f415798edb98ada3575
Tarik366 <tariky113@gmail.com>
2026-08-21 16:28:58 +0300

    Fixed all problems with the Stylesheets and header

--- include/interpretter.h
+++ include/interpretter.h
@@ -1,65 +1,15 @@
 #pragma once
 
-#include <string>
 #include <map>
 #include <regex>
+#include <string>
 #include <string_view>
 #include <ranges>
 #include <Magick++.h>
 
 #include <iostream>
 
-// FIXME: Move this enums to a special file
-enum textDirection
-{
-    ltr,
-    rtl,
-    ttb,
-    btt
-};
-
-enum direction
-{
-    left, center, right
-};
-
-class styleSheet
-{
-    std::string prefix;
-    std::string* fontFamilies;
-    int fontSize;
-    direction spacing;
-    int outlineSize;
-    Magick::Color outlineColor;
-
-public:
-    styleSheet();
-    ~styleSheet();
-
-private:
-
-};
-
-styleSheet::styleSheet()
-{
-}
-
-styleSheet::~styleSheet()
-{
-}
-
-std::string trim(const std::string& str) {
-    // Removes all whitespaces
-    const std::string whitespace = "\t\n\r\f\v";
-    const auto strBegin = str.find_first_not_of(whitespace);
-
-    if (strBegin == std::string::npos) return ""; // Only whitespace found
-
-    const auto strEnd = str.find_last_not_of(whitespace);
-    const auto strRange = strEnd - strBegin + 1;
-
-    return str.substr(strBegin, strRange);
-}
+#include <stylesheets.h>
 
 std::string removeComments(std::string styleText) {
     // find all comment delimeters and pop until new-line character
@@ -69,46 +19,76 @@ std::string removeComments(std::string styleText) {
     return trim(std::regex_replace(styleText, comment, ""));
 }
 
-std::map<std::string, std::string> readCascading(std::string styleText) {
-    // split text or just use regex, i don't know man
-    const std::map<std::string, std::string> result;
-    for (const auto line : std::views::split(styleText, ';')) {
-        // std::cout << std::views::split(line, ':')[0];
-    }
-    return result;
-}
-
-std::string parseBody(std::string bcscript) {
-    std::regex pattern("<header>(?<header>[\s\S]*)</header>[\s\S]*<body>(?<body>[\s\S]*)</body>");
-    std::smatch matches;
-    if (std::regex_search(bcscript, matches, pattern)) {
-        std::cout << matches[4];
-    }
-}
-
-class Body {
-    Body(std::string bodyText) {
-
-    }
-};
-
 class Header {
 public:
     std::string title;
-    textDirection paging;
-    textDirection lineDirection;
-    textDirection wordingDirection;
+    textDirection paging = ltr;
+    textDirection lineDirection = ttb;
+    textDirection wordingDirection = ltr;
 
     std::map<std::string, styleSheet> styles;
 
-    Header(std::string headerText) {
-        std::regex pattern("<(?<tag>\w+)(?:\s+(?<settings>[^>]*))?>(?<content>[\s\S]*?)<\/\k<tag>>");
+    Header() {}
 
-        std::smatch matches;
-        if (std::regex_search(headerText, matches, pattern)) {
-            std::cout << matches[4];
+    explicit Header(std::string headerText) {
+        // Grup 1 = tag, Grup 2 = settings (opsiyonel), Grup 3 = content
+        std::regex pattern("<(?:(\\w+))(?:\\s+(?:([^>]*)))?>(?:([\\s\\S]*?))</\\1>");
+
+        enum Group { TAG = 1, SETTINGS = 2, CONTENT = 3 };
+
+        auto begin = std::sregex_iterator(headerText.begin(), headerText.end(), pattern);
+        auto end = std::sregex_iterator();
+
+        for (auto it = begin; it != end; ++it) {
+            const std::smatch& m = *it;
+
+            std::string tag = m[TAG].str();
+            std::string content = m[CONTENT].str();
+
+            // TODO: Must be rewritten to be more human-friendly readable form
+            if (tag == "title") {
+                title = content;
+            } else if (tag == "order") {
+                auto orderings = readCascading(content);
+
+                paging = stringToDirections(orderings["paging"]);
+                lineDirection = stringToDirections(orderings["line-direction"]);
+                wordingDirection = stringToDirections(orderings["wording-direction"]);
+            } else if (tag == "style") {
+                styles = readStyleTag(content);
+            }
+
+            if (m[SETTINGS].matched) {
+                break;
+            }
+
         }
     };
+};
+
+struct typeSet {
+    std::string line;
+    std::vector<int> positon;
+};
+
+class Body {
+public:
+    // TODO: Add .ass like inline text-rendering
+
+    // Stores the text data with the pointer of the stylesheet
+    // TODO: Create an exclusive struct for line
+    std::list<std::string> typeSet;
+
+    // Header header;
+
+    Body() {}
+
+    explicit Body(std::string bodyText, Header* constructHeader) {
+        for (const auto subrange : std::views::split(bodyText, "\n")) {
+            std::string_view line = std::string_view(subrange.begin(), subrange.end());
+            
+        }
+    }
 };
 
 class BCScript {
@@ -116,19 +96,36 @@ class BCScript {
     Body body;
 
     BCScript(std::string bcscript) {
-        std::regex pattern("<header>(?<header>[\s\S]*)</header>[\s\S]*<body>(?<body>[\s\S]*)</body>");
+        std::regex pattern("<header>(?<header>[\\s\\S]*)</header>[\\s\\S]*<body>(?<body>[\\s\\S]*)</body>");
         std::smatch matches;
         if (std::regex_search(bcscript, matches, pattern)) {
-            header = Header(matches[0].str());
-            body = Body(matches[1].str());
+            header = Header::Header(matches[0].str());
+            body = Body::Body(matches[1].str(), &header);
         }
     }
 };
 
+std::string parseBody(std::string bcscript) {
+    std::regex pattern("<header>([\\s\\S]*)</header>[\\s\\S]*<body>([\\s\\S]*)</body>");
+    auto begin = std::sregex_iterator(bcscript.begin(), bcscript.end(), pattern);
+    auto end = std::sregex_iterator();
+
+
+    for (auto it = begin; it != end; ++it) {
+        const std::smatch& m = *it;
+
+        Header header = Header::Header(m[1].str());
+        Body body = Body::Body(m[2].str(), &header);
+
+    }
+
+    return "";
+}
+
 class Episode
 {
 private:
-    BCScript script;
+    // BCScript script;
     // Pageset pages;
 
 };
---
+++ include/stylesheets.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <regex>
+#include <string_view>
+#include <ranges>
+#include <list>
+#include <cmath>
+#include <iomanip>
+
+#include <Magick++.h>
+
+#include <styleConstants.h>
+#include <stringOperations.h>
+
+// Turn any "18pt", "10%" like dimensions to pixels to be can processed by imageMagick
+int turnToPixels(std::string dimension) {
+    std::regex pattern("(\\d+)(\\w+)");
+
+    auto begin = std::sregex_iterator(dimension.begin(), dimension.end(), pattern);
+    auto end = std::sregex_iterator();
+
+    for (auto it = begin; it != end; ++it) {
+        const std::smatch& m = *it;
+
+        if (m[2].str() == "px") {
+            return std::stoi(m[1].str());
+        }
+        else if (m[2].str() == "pt") {
+            return std::lround(std::stof(m[1].str()) * 1.3333f);
+        }
+    }
+}
+
+class styleSheet
+{
+public:
+    std::string prefix;
+    // TODO: Replace with new font system for optimizations
+    std::list<std::string> fontFamilies = {};
+    int fontSize = 16;
+    Magick::Color color = "#000000";
+    direction spacing = center;
+    int outlineSize = 0;
+    Magick::Color outlineColor = Magick::Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+    styleSheet(std::string t_prefix, std::list<std::string> t_fontFamilies, int t_fontSize, Magick::Color t_color, direction t_spacing, int t_outlineSize, Magick::Color t_outlineColor) : prefix{ std::move(t_prefix) }, fontFamilies{ std::move(t_fontFamilies) }, fontSize{ t_fontSize }, color{ std::move(t_color) }, spacing{ t_spacing }, outlineSize{ t_outlineSize }, outlineColor{ std::move(t_outlineColor) } {}
+
+    explicit styleSheet(std::string styleText) {
+
+        auto cascades = readCascading(styleText);
+
+        if (!cascades.empty()) {
+            prefix = cascades["prefix"];
+            for (const auto subrange : std::views::split(cascades["font"], ", ")) {
+                fontFamilies.emplace_back(subrange.begin(), subrange.end());
+            }
+            fontSize = 16;
+            fontSize = turnToPixels(cascades["font-size"]);
+            color = cascades["color"];
+            spacing = strToDir(cascades["spacing"]);
+            outlineSize = turnToPixels(cascades["outline-size"]);
+
+            try {
+                Magick::Color parsedColor(cascades["outline-color"]);
+            }
+            catch (const Magick::Exception& error) {
+                std::cerr << "Error parsing color string: " << error.what() << std::endl;
+            }
+
+        }
+    };
+
+};
+
+std::map<std::string, styleSheet> readStyleTag(std::string styleText) {
+    std::map<std::string, styleSheet> result;
+
+    std::regex solvePattern("(\\w+) (?:{([\\s\\S]+?)})");
+
+    auto begin = std::sregex_iterator(styleText.begin(), styleText.end(), solvePattern);
+    auto end = std::sregex_iterator();
+
+    for (auto it = begin; it != end; ++it) {
+        const std::smatch& m = *it;
+        
+        result.emplace(m[1].str(), styleSheet::styleSheet(m[2].str()));
+    }
+
+    return result;
+}
+
