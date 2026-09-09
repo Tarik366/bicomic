60c3199e4606f440b4c6e1dfe75e06471e15b546
Tarik366 <tariky113@gmail.com>
2026-09-09 15:57:29 +0300

    PSD: Reading Effect descriptors accomplished.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from psd_tools import PSDImage, constants, color_convert
+from psd_tools import PSDImage, constants, color_convert, api
+from psd_tools.api.effects import _Effect
+import psd_tools.psd.descriptor
+import json
+from json import JSONEncoder
 
-psd = PSDImage.open('test.psd')
+psd = PSDImage.open('unicorn-controller.psd')
 psd.composite().save('example.png')
 
 class Line:
@@ -17,6 +21,14 @@ class BCScript:
     styles: dict[dict]
     lines: Line
 
+"""class BEffect(_Effect):
+    def to_json(self):
+        """
+
+class EncodeStudent(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
 with open("export.bcs","w") as BCScript:
 
     # list of fonts in the psd file for font gathering process
@@ -24,16 +36,43 @@ with open("export.bcs","w") as BCScript:
 
     for layer in psd:
         if layer.kind == "type":
-            print(f"{layer.size}, {layer.offset}")
-            print(layer.text)
+            BCScript.write(f"{layer.size}, {layer.offset};\n")
+            BCScript.write(f"{layer.text};\n")
+
+            print(layer.effects.items)
+            for effect in layer.effects.items:
+                copy_of_descriptor = {}
+                for key, val in effect.__dict__["descriptor"].items():
+
+                    if isinstance(val, psd_tools.psd.descriptor.Bool):
+                        copy_of_descriptor[key.decode("utf-8")] = bool(val.value)
+
+                    elif isinstance(val, psd_tools.psd.descriptor.Enumerated):
+                        copy_of_descriptor[key.decode("utf-8")] = val.get_name()
+                        
+                    elif isinstance(val, psd_tools.psd.descriptor.UnitFloat):
+                        copy_of_descriptor[key.decode("utf-8")] = val.value
+                        copy_of_descriptor[f"{key.decode("utf-8")}_unit"] = val.unit.value.decode()
+
+                    elif isinstance(val, psd_tools.psd.descriptor.Descriptor):
+                        clr = {}
+                        for chan, col in val.items():
+                            clr[chan.decode()] = col.value
+                        copy_of_descriptor[key.decode("utf-8")] = clr
+                        
+                    else:
+                        copy_of_descriptor[key.decode("utf-8")] = val
+                        
+                print(json.dumps(copy_of_descriptor))
+                json.dump(copy_of_descriptor, BCScript)
 
             text = layer.engine_dict['Editor']['Text'].value
 
             ts = layer.typesetting
             for paragraph in ts:
                 stylesUsing = {}
-                print(constants.Justification(paragraph.style.justification).name)
+                BCScript.write(constants.Justification(paragraph.style.justification).name)
                 for run in paragraph.runs:
-                    print(run.text, run.style.font_name, f"{round(run.style.font_size * 1.3333)}px", run.style.fill_color, run.style.stroke_color)
+                    BCScript.write(f"\n{run.text};{run.style.font_name};{round(run.style.font_size * 1.3333)};{run.style.fill_color};{run.style.stroke_color}\n\n")
 
                     # TODO: Convert text layers into bcscript file
